5958af0898de393b856f0c81dbc819b4867ff043
Rowan Harpold <16273005@edu.cbe.ab.ca>
2024-02-08 14:04:54 -0700

    Working on Insertion sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -8,8 +8,36 @@ function bubbleSort(array) {
     ** Repeat the above process until you get through an entire
     ** cycle without needing any swaps
     */
-
+    if(array.length == 0){
+		return array
+	}
+	let inOrder = false
+	while(!inOrder){
+		for (let i = 0; i < array.length - 1; i++) {
+			if(array[i] > array[i + 1]){
+				let temp = array[i];
+				array[i] = array[i+1];
+				array[i+1] = temp;
+			}	
+		}
+		if(checkInOrder(array)){
+			inOrder = true
+			break;
+		}
+	}
 	return array;
+}
+
+function checkInOrder(arr){
+	let numOrder = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if(arr[i] <= arr[i + 1]){
+			numOrder = numOrder + 1
+		}	
+	}
+	if(numOrder == arr.length - 1){
+		return true;
+	}
 }
 
 module.exports = bubbleSort;
--- insertion-sort.js
+++ insertion-sort.js
@@ -1,39 +1,31 @@
 function insertionSort(array) {
+	if(array.length == 0){
+		return array;
+		
+	}
+	let newArr = [array[0]];
+	array.splice(0, 1);	
+	let inOrder = false
+	let length = array.length
+	while(!inOrder){
+		newArr.unshift(array[0])
+		array.splice(0, 1);
+		bubbleSort(newArr)
+		if(newArr.length == length){
+			inOrder = true;
+			break;
+		}
+	}
+	return newArr;
+
 	/*
 	 ** For each element in the array, swap it with the element
 	 ** before it over and over as long as the element before it
 	 ** is bigger
 	 */
-	if(array.length == 0){
-		return array
-	}
-	let inOrder = false
-	while(!inOrder){
-		for (let i = 0; i < array.length - 1; i++) {
-			if(array[i] > array[i + 1]){
-				let temp = array[i];
-				array[i] = array[i+1];
-				array[i+1] = temp;
-			}	
-		}
-		if(checkInOrder(array)){
-			inOrder = true
-			break;
-		}
-	}
-	return array;
 }
 
-function checkInOrder(arr){
-	let numOrder = 0;
-	for (let i = 0; i < arr.length; i++) {
-		if(arr[i] <= arr[i + 1]){
-			numOrder = numOrder + 1
-		}	
-	}
-	if(numOrder == arr.length - 1){
-		return true;
-	}
-}
+
+
 
 module.exports = insertionSort;
